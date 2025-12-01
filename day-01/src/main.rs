39cc1deaa1adfc47a4ad0d946bd6e4229e25e12e
Adrian Axelsson <28781849+AdrianAxelsson@users.noreply.github.com>
2025-12-01 13:55:58 +0100

    refactor day-01

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() {
-    let input_file = File::open("data/input.txt").unwrap();
+fn main() -> std::io::Result<()> {
+    let input_file = File::open("data/input.txt")?;
     let reader = BufReader::new(input_file);
 
     let mut position: i32 = 50;
@@ -10,26 +10,22 @@ fn main() {
     let mut zero_count: i32 = 0;
 
     for line in reader.lines() {
-        let line = line.unwrap();
+        let line = line?;
         let (direction, value_str) = line.split_at(1);
         let value: i32 = value_str.parse().unwrap();
         for _ in 0..value {
             position += if direction == "L" { -1 } else { 1 };
 
-            position = match position {
-                -1 => 99,
-                100 => 0,
-                _ => position,
-            };
-
-            if position == 0 {
+            if position.rem_euclid(100) == 0 {
                 zero_pass_count += 1
             }
         }
-        if position == 0 {
+        if position.rem_euclid(100) == 0 {
             zero_count += 1
         }
     }
     println!("Part 1: {}", zero_count);
     println!("Part 2: {}", zero_pass_count);
+
+    Ok(())
 }
